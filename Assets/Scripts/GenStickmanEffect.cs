4f567dc5caa05cc10fe2ff83c2f9b7011eb1fadf
minhkhoi1026 <minhkhoi1026@gmail.com>
2022-12-20 05:42:42 +0700

    config camera

---
+++ Assets/Scripts/GenStickmanEffect.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+// reference: https://library.vuforia.com/getting-started/vuforia-engine-api-unity#attach-content-to-a-target
+public class GenStickmanEffect : DefaultObserverEventHandler
+{
+    ImageTargetBehaviour mImageTarget;
+    public GameObject myModelPrefab;
+    [SerializeField] private float startDropHeight;
+    [SerializeField] private float relativeScale;
+
+    private void Awake()
+    {
+        mImageTarget = GetComponent<ImageTargetBehaviour>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        InstantiateStickman();
+    }
+
+    void InstantiateStickman()
+    {
+        if (myModelPrefab != null)
+        {
+            GameObject myModelTrf = Instantiate(myModelPrefab);
+            // myModelTrf.transform.parent = mImageTarget.transform;
+            myModelTrf.transform.localPosition = new Vector3(0f, startDropHeight, 0f);
+            myModelTrf.transform.rotation = Quaternion.identity;
+            myModelTrf.transform.localScale = new Vector3(relativeScale, relativeScale, relativeScale);
+        }
+    }
+}
