1fcea2b70835e46999391c9a4e28538530e8e013
binhlecong <congbinh21012001@gmail.com>
2022-12-20 13:15:52 +0700

    edit EffectTrigger.cs, edit prefabs

--- Assets/Scripts/EffectTrigger.cs
+++ Assets/Scripts/EffectTrigger.cs
@@ -2,15 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Effect
+{
+    JUMP,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    SPIKES
+}
+
 public class EffectTrigger : MonoBehaviour
 {
-    public Direction direction;
+    public Effect effect;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other == null) return;
         Stickman o = other.GetComponent<Stickman>();
         if (o == null) return;
-        o.changeDirection(direction);
+        ApplyEffect(o);
+    }
+
+    private void ApplyEffect(Stickman stickman)
+    {
+        switch (effect)
+        {
+            case Effect.UP:
+                stickman.changeDirection(Direction.UP);
+                break;
+            case Effect.DOWN:
+                stickman.changeDirection(Direction.DOWN);
+                break;
+            case Effect.LEFT:
+                stickman.changeDirection(Direction.LEFT);
+                break;
+            case Effect.RIGHT:
+                stickman.changeDirection(Direction.RIGHT);
+                break;
+            case Effect.JUMP:
+                stickman.jump();
+                break;
+            case Effect.SPIKES:
+                // Deal emotional damage to stickman
+                break;
+        }
     }
 }
