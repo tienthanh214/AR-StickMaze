6258a5fb7df3d44e4ff45b2cc16c760c10a9e5c5
binhlecong <congbinh21012001@gmail.com>
2023-01-07 16:45:19 +0700

    fix bug: update stickman count bar

--- Assets/Scripts/GUI/StickmanStatusPanel.cs
+++ Assets/Scripts/GUI/StickmanStatusPanel.cs
@@ -6,30 +6,44 @@ public class StickmanStatusPanel : MonoBehaviour
 {
     private static int MAX_STICKMAN_COUNT = 8;
     public GameObject stickmanIconPrefab;
-    private int currentStickmanCount = MAX_STICKMAN_COUNT;
+    private List<GameObject> stickmanIconList = new List<GameObject>();
 
     void Start()
     {
-        setCount(6);
+        updateCount(3);
     }
 
-    void setCount(int count)
+    public void updateCount(int count)
     {
-        currentStickmanCount = count;
-        updateIcons();
-    }
+        if (count < 0)
+            count = 0;
+        else if (count > MAX_STICKMAN_COUNT)
+            count = MAX_STICKMAN_COUNT;
 
-    void updateIcons()
-    {
-        for (int i = 0; i < currentStickmanCount; ++i)
+        int currentCount = stickmanIconList.Count;
+
+        if (currentCount < count)
         {
-            Vector3 iconPosition = transform.position;
-            iconPosition.x += i * 20  + 15;
-            iconPosition.y -= 15;
-            GameObject stickmanIcon = Instantiate(stickmanIconPrefab) as GameObject;
-            stickmanIcon.transform.parent = transform;
-            stickmanIcon.transform.position = iconPosition;
-            Debug.Log("tao roi ma");
+            for (int i = currentCount; i < count; ++i)
+            {
+                Vector3 iconPosition = transform.position;
+                iconPosition.x += i * 20 + 15;
+                iconPosition.y -= 15;
+                GameObject stickmanIcon = Instantiate(stickmanIconPrefab);
+                stickmanIcon.transform.SetParent(transform);
+                stickmanIcon.transform.position = iconPosition;
+                stickmanIconList.Add(stickmanIcon);
+            }
+        }
+        else
+        {
+            for (int i = count; i < currentCount; ++i)
+            {
+                int lastIndex = stickmanIconList.Count - 1;
+                GameObject stickmanIcon = stickmanIconList[lastIndex];
+                stickmanIconList.RemoveAt(lastIndex);
+                Destroy(stickmanIcon);
+            }
         }
     }
 }
