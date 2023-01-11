1a42530451d440f69d760ac1d09c1e43cdbbf2d3
Thành Nguyễn Đăng Tiến <tienthanhcute214@gmail.com>
2023-01-11 21:14:26 +0700

    feat: attack stickman to grid for scale

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	private static GameManager _instance = null;
-
+	public GameObject grid;
 	public int nInitialStickman = 0;
 	private int coin = 0;
 
@@ -51,6 +51,7 @@ public class GameManager : MonoBehaviour
 			float biasZ = Random.Range(-0.05f, 0.05f);
 			GameObject myModelTrf = Instantiate(stickman, new Vector3(position.x + biasX, position.y, position.z + biasZ), Quaternion.identity) as GameObject;
 			// myModelTrf.transform.localScale = scaleVector;
+			myModelTrf.transform.SetParent(grid.transform);
 			stickmanAlive.Add(myModelTrf);
 			Instantiate(startGame, myModelTrf.transform);
 			// show particle
